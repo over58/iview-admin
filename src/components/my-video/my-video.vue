b37a27b8276b0c9ab625e17dc96e6ce6db12d544
yongchao9 <2658553345@qq.com>
2019-11-23 13:31:49 +0800

    监听video播放结束的事件

---
+++ src/components/my-video/my-video.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+  {{played}}
+  <video ref="videoPlayer" :src="url" :poster="poster" controls :autoplay="autoplay">
+  </video>
+</div>
+</template>
+
+<script>
+let vm = null
+export default {
+  props: {
+    url : {
+      type: String,
+      default: 'https://www.runoob.com/try/demo_source/movie.mp4'
+    },
+    poster: {
+      type: String,
+      default: ''
+    },
+    autoplay: {
+      type: Boolean,
+      default: false
+    },
+    loop: {
+      type :Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      isPlaying: false,
+      played: false
+    }
+  },
+  mounted () {
+    vm = this
+    this.$refs.videoPlayer.addEventListener('ended', this.videoPlayed)
+  },
+  beforeDestroy () {
+    this.$refs.videoPlayer.removeEventListener('ended', this.videoPlayed)
+  },
+  methods: {
+    videoPlayed () {
+      this.played = true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/video.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <my-video></my-video>
+  </div>
+</template>
+
+<script>
+import myVideo from '@/components/my-video/my-video.vue'
+export default {
+  components: {
+    myVideo
+  }
+}
+</script>
+
+<style>
+
+</style>
