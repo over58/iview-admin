5354588c5fc2ad65c2de0b239fa64e9a672e73b8
xuyongchao <xuyongchao6585@ipalfish.com>
2019-12-25 14:43:37 +0800

    update

--- src/router/afterRouter.js
+++ src/router/afterRouter.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
-import iView from "iview";
 import store from "@/store";
 let vm = new Vue({ store });
 export default function(router) {
   router.afterEach(to => {
     // 将访问记录推入栈中
     vm.$store.commit("pushHistory", to);
-    iView.LoadingBar.finish();
   });
 }
--- src/router/beforeRouter.js
+++ src/router/beforeRouter.js
@@ -1,8 +1,6 @@
-import iView from "iview";
 import localStore from "api/localStore";
 export default function(router) {
   router.beforeEach((to, from, next) => {
-    iView.LoadingBar.start();
     if (to.query.token) {
       localStore.setToken(to.query.token);
       delete to.query.token;
